1f97da332eda7b229a9e84906705f65c359f9046
rafiti12 <rafiti12@o2.pl>
2022-01-06 18:16:58 +0100

    1.5. Testy, diagramy, dokumentacja i *gwiazdki Utworzenie dokumentacji klasy IntLinkedList oraz dodanie "TODO"

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,10 +1,18 @@
 package edu.kis.vh.nursery.list;
 
+/**
+ * Klasa robiąca za listę dwukierunkową z obiektami klasy Node
+ */
+
 public class IntLinkedList {
 
 	private Node last;
 	private int index;
 
+	/**
+	 * Dodanie nowego elemntu do klasy Node
+	 * @param i wartość do ustawienia w obiekcie Node
+	 */
 	public void push(int i) {
 		if (last == null)
 			last = new Node(i);
@@ -15,20 +23,38 @@ public class IntLinkedList {
 		}
 	}
 
+	// TODO: uzupełnić metodę
+	/**
+	 * sprawdzenie czy lista jest pusta
+	 * @return true jeśli lista jest pusta, false w przeciwnym wypadku
+	 */
 	public boolean isEmpty() {
 		return last == null;
 	}
 
+	// TODO: uzupełnić metodę
+	/**
+	 * sprawdzenie czy lista jest pełna
+	 * @return true jeśli lista jest pusta, false w przeciwnym wypadku
+	 */
 	public boolean isFull() {
 		return false;
 	}
 
+	/**
+	 * zwraca pierwszy elemnt listy
+	 * @return pierwszy elemnt listy lub -1 jeśli lista jest pusta
+	 */
 	public int top() {
 		if (isEmpty())
 			return -1;
 		return last.getValue();
 	}
 
+	/**
+	 * usuwa pierwszy elemnt listy
+	 * @return wartość usuniętego elementu lub -1 jeśli lista jest pusta
+	 */
 	public int pop() {
 		if (isEmpty())
 			return -1;
